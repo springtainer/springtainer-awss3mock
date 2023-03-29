5fe29eebc89869064160a0c36af81e73c9d19a7f
Martin Schumacher <mschumacher@avides.com>
2023-03-29 21:00:27 +0200

    LIBS-1227 - updated dependencies/image (compatibility to apple-chips) (#15)

--- src/test/java/com/avides/springboot/springtainer/awss3mock/AwsS3MockPropertiesTest.java
+++ src/test/java/com/avides/springboot/springtainer/awss3mock/AwsS3MockPropertiesTest.java
@@ -10,10 +10,10 @@ public class AwsS3MockPropertiesTest
     @Test
     public void testDefaults()
     {
-        AwsS3MockProperties properties = new AwsS3MockProperties();
+        var properties = new AwsS3MockProperties();
         assertTrue(properties.isEnabled());
         assertEquals(30, properties.getStartupTimeout());
-        assertEquals("adobe/s3mock:2.4.7", properties.getDockerImage());
+        assertEquals("adobe/s3mock:2.11.0", properties.getDockerImage());
         assertEquals(9090, properties.getEndpointHttpPort());
         assertEquals(9191, properties.getEndpointHttpsPort());
     }
